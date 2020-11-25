2b89c44ff5fa3a914c76ef23d1970d37df68e576
d3mah <13hidonik>
2020-11-25 21:38:27 +0500

    #8 sematic

--- infosync-ui/src/index.js
+++ infosync-ui/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
